976427f55fe59d89ec64b6624651cc0b33698d4e
Asa Rogers <cyberasasoftware@gmail.com>
2024-10-31 21:15:21 -0500

    remove install

--- build/turtle_brick/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/asaace00/repo/me495/hw2/src/homework2/install/turtle_brick'
--- build/turtle_brick_interfaces/turtle_brick_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/asaace00/repo/me495/hw2/src/homework2/build/turtle_brick_interfaces/turtle_brick_interfaces__py
-# Build directory: /home/asaace00/repo/me495/hw2/src/homework2/build/turtle_brick_interfaces/turtle_brick_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
